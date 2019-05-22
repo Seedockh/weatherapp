05229f1c8f5594a54c06509ffc9b78fcbae59b29
CrackyStudio <antoine.nivoy@gmail.com>
2019-05-22 10:03:20 +0200

    Navigation to city added

---
+++ weatherapp/weatherapp/CityViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CityViewController.swift
+//  weatherapp
+//
+//  Created by Antoine Nivoy on 22/05/2019.
+//  Copyright © 2019 efrei. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CityViewController: UIViewController {
+
+    var city: MKPointAnnotation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
